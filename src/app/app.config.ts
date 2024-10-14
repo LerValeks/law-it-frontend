55fda7bfd79586459d7f50930cb75d35f2b15482
Oleksandr Valuyskov <aleksvaluyskov@gmail.com>
2024-10-14 15:04:12 +0200

    Initial Commit

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,12 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import {ApplicationConfig, provideZoneChangeDetection} from '@angular/core';
+import {provideRouter} from '@angular/router';
 
-import { routes } from './app.routes';
+import {routes} from './app.routes';
+import {initializeApp, provideFirebaseApp} from '@angular/fire/app';
+import {getAuth, provideAuth} from '@angular/fire/auth';
+import {getMessaging, provideMessaging} from '@angular/fire/messaging';
+import {environments} from '../environments/environments.prod';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [provideZoneChangeDetection({eventCoalescing: true}), provideRouter(routes), provideFirebaseApp(() => initializeApp(environments.firebase)), provideAuth(() => getAuth()), provideMessaging(() => getMessaging())]
 };
---
+++ src/environments/environments.prod.ts
@@ -0,0 +1,14 @@
+export const environments = {
+  production: true,
+
+  firebase: {
+    "projectId": "fir-poc-b1a3d",
+    "appId": "1:825417668199:web:ea21674e3490208e64e6c2",
+    "databaseURL": "https://fir-poc-b1a3d-default-rtdb.europe-west1.firebasedatabase.app",
+    "storageBucket": "fir-poc-b1a3d.appspot.com",
+    "apiKey": "AIzaSyA6cs2OtFMvAGmNOQt-pZTAbsQz3QcoPUs",
+    "authDomain": "fir-poc-b1a3d.firebaseapp.com",
+    "messagingSenderId": "825417668199",
+    "measurementId": "G-LB41SBXLT5"
+  }
+}
